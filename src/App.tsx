f245d17993daaa77f8516ffc83fe17f597af0ce7
ReynierMatth <matthieur.pro@outlook.com>
2023-01-24 21:55:09 +0100

    react browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
+import {Navbar} from "./core/organism/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
     return (
-        <div className="min-h-screen flex justify-center items-center">
-            <h1 className="text-3xl font-bold text-blue-600">
-                Install & Setup Vite + React + Typescript + Tailwind CSS 3
-            </h1>
-        </div>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route index element={<div>Home</div>}/>
+                <Route path={'/my-profile'} element={<div>Mon profil</div>}/>
+                <Route path={'/about'} element={<div>About</div>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 

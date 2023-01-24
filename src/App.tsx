a934523783133c831cbbd017d6caba7d68d7ab7b
ReynierMatth <matthieur.pro@outlook.com>
2023-01-24 22:09:54 +0100

    create page + set navigate in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {Navbar} from "./core/organism/Navbar";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Landing} from "./presentation/pages/Landing/Landing";
+import {MyProfile} from "./presentation/pages/MyProfile/MyProfile";
+import {About} from "./presentation/pages/About/About";
 
 function App() {
 
@@ -8,9 +11,9 @@ function App() {
         <BrowserRouter>
             <Navbar/>
             <Routes>
-                <Route index element={<div>Home</div>}/>
-                <Route path={'/my-profile'} element={<div>Mon profil</div>}/>
-                <Route path={'/about'} element={<div>About</div>}/>
+                <Route index element={<Landing />}/>
+                <Route path={'/my-profile'} element={<MyProfile />}/>
+                <Route path={'/about'} element={<About />}/>
             </Routes>
         </BrowserRouter>
     )

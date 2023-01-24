790a8fdfe5027dc708f2ec618d73511ced690acd
ReynierMatth <matthieur.pro@outlook.com>
2023-01-24 22:36:44 +0100

    add card

--- src/presentation/pages/Landing/Landing.tsx
+++ src/presentation/pages/Landing/Landing.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Card} from "../../../core/molecule/Card";
 
 export const Landing: React.FC = () => {
     return (
-        <div>
-            <h1>Landing</h1>
+        <div className={"w-full flex items-center justify-center pt-10"}>
+            <Card/>
         </div>
     )
 }
